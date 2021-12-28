a8321cec14e3dcf7be3cba6b14c3975c2f6e398d
Kate Granstrom <kategranstrom2000@gmail.com>
2021-12-27 21:10:21 -0800

    add start script, bug fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-const PORT = process.event.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
 })
